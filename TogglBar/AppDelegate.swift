14fb995e22b176a0bb5121f8ae4cbd32228df9cf
Felipe Augusto Sviaghin Ferri <felipe.ferri@gmail.com>
2017-04-01 21:50:01 -0300

    Initial Commit

---
+++ TogglBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TogglBar
+//
+//  Created by Felipe Augusto Sviaghin Ferri on 1/4/17.
+//  Copyright © 2017 Felipe Augusto Sviaghin Ferri. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
